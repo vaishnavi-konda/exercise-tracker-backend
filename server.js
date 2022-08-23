d2cdfcf4fec4703619df1871578941d18ca10c07
Vaishnavi Konda <kvaishnavi026@gmail.com>
2022-08-23 11:55:38 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+app.use(express.json());
+app.use(cors());
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, function (req, res) {
+  console.log(`Listening on port ${port}`);
+});
